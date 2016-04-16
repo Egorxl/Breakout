5603d6c2e883676dc3ded0eed948a356b79693c3
егор Емельянов < >
2016-04-16 17:58:40 +0300

    probably broke something)

--- src/BreakoutGame.java
+++ src/BreakoutGame.java
@@ -1,7 +1,8 @@
 import java.awt.Color;
 import java.awt.event.*;
-
 import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
 import acm.program.*;
 
 public class BreakoutGame extends GraphicsProgram {
@@ -9,13 +10,15 @@ public class BreakoutGame extends GraphicsProgram {
 	private static final int WINDOW_WIDTH = 400;
 	private static final int WINDOW_HIGHT = 600;
 
-	private static final int NUMBER_OF_BLOCKS = 10;
-	private static final int BLOCK_WIDTH = (WINDOW_WIDTH - (NUMBER_OF_BLOCKS - 1)
+	private static final int NUMBER_OF_COLOMNES = 10;
+	private static final int BLOCK_WIDTH = (WINDOW_WIDTH - (NUMBER_OF_COLOMNES - 1)
 			* SPACE_BETWEEN_BLOCKS)
-			/ NUMBER_OF_BLOCKS;
+			/ NUMBER_OF_COLOMNES;
 	private static final int BLOCK_HIGHT = 8;
-	private static final int NUMBER_OF_LINES = 10;
+	private static final int NUMBER_OF_RAWS = 10;
 	private static final int BLOCKS_TOP_OFFSET = 70;
+
+	GRect blocks[][] = new GRect[NUMBER_OF_COLOMNES][NUMBER_OF_RAWS];
 
 	private static final int PADDLE_WIDTH = 60;
 	private static final int PADDLE_HIGHT = 10;
@@ -23,15 +26,24 @@ public class BreakoutGame extends GraphicsProgram {
 
 	private static final int COLLIDER_RADIUS = 10;
 
-	private static final int SPEED = 1;
+	private static final int SPEED = 5;
 
 	private int blockX = 0;
 	private int blockY = BLOCKS_TOP_OFFSET;
 
 	int numberOfBlocks = 0;
 
-	GLabel info = new GLabel(
-			"чтобы начать игру, нажмите пробел \n чтобы запустить шар, нажмите левый SHIFT");
+	GLabel info = new GLabel("чтобы начать игру, нажмите пробел");
+
+	int colliderX = (WINDOW_WIDTH - PADDLE_WIDTH) / 2 + PADDLE_WIDTH / 2
+			- COLLIDER_RADIUS;
+	int colliderY = WINDOW_HIGHT - PADDLE_BOTTOM_OFFSET - COLLIDER_RADIUS * 2;
+	GOval collider = new GOval(COLLIDER_RADIUS * 2, COLLIDER_RADIUS * 2);
+
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	int randomNumberForCollderDirection = rgen.nextInt(25, 155);
+	int colliderDirectionY = (randomNumberForCollderDirection);
+	int colliderDirectionX = (180 - randomNumberForCollderDirection);
 
 	int paddleX = (WINDOW_WIDTH - PADDLE_WIDTH) / 2;
 	int paddleY = WINDOW_HIGHT - PADDLE_BOTTOM_OFFSET;
@@ -62,50 +74,56 @@ public class BreakoutGame extends GraphicsProgram {
 	}
 
 	private void setBlocks() {
-		for (int i = 0; i < NUMBER_OF_BLOCKS; i++) {
-			for (int f = 0; f < NUMBER_OF_LINES / 5; f++) {
+		// TODO переписать на более простой вариант
+		for (int i = 0; i < NUMBER_OF_COLOMNES; i++) {
+			for (int f = 0; f < NUMBER_OF_RAWS / 5; f++) {
 				GRect block = new GRect(BLOCK_WIDTH, BLOCK_HIGHT);
 				block.setColor(Color.RED);
 				block.setFilled(true);
 				block.setFillColor(Color.RED);
 				add(block, blockX, blockY);
 				numberOfBlocks++;
+				blocks[i][f] = block;
 				blockY += BLOCK_HIGHT + SPACE_BETWEEN_BLOCKS;
 			}
-			for (int f = 0; f < NUMBER_OF_LINES / 5; f++) {
+			for (int f = 0; f < NUMBER_OF_RAWS / 5; f++) {
 				GRect block = new GRect(BLOCK_WIDTH, BLOCK_HIGHT);
 				block.setColor(Color.ORANGE);
 				block.setFilled(true);
 				block.setFillColor(Color.ORANGE);
 				add(block, blockX, blockY);
 				numberOfBlocks++;
+				blocks[i][f] = block;
 				blockY += BLOCK_HIGHT + SPACE_BETWEEN_BLOCKS;
 			}
-			for (int f = 0; f < NUMBER_OF_LINES / 5; f++) {
+			for (int f = 0; f < NUMBER_OF_RAWS / 5; f++) {
 				GRect block = new GRect(BLOCK_WIDTH, BLOCK_HIGHT);
 				block.setColor(Color.YELLOW);
 				block.setFilled(true);
 				block.setFillColor(Color.YELLOW);
 				add(block, blockX, blockY);
 				numberOfBlocks++;
+				blocks[i][f] = block;
 				blockY += BLOCK_HIGHT + SPACE_BETWEEN_BLOCKS;
 			}
-			for (int f = 0; f < NUMBER_OF_LINES / 5; f++) {
+			for (int f = 0; f < NUMBER_OF_RAWS / 5; f++) {
 				GRect block = new GRect(BLOCK_WIDTH, BLOCK_HIGHT);
 				block.setColor(Color.GREEN);
 				block.setFilled(true);
 				block.setFillColor(Color.GREEN);
 				add(block, blockX, blockY);
 				numberOfBlocks++;
+				blocks[i][f] = block;
 				blockY += BLOCK_HIGHT + SPACE_BETWEEN_BLOCKS;
 			}
-			for (int f = 0; f < NUMBER_OF_LINES / 5; f++) {
+			for (int f = 0; f < NUMBER_OF_RAWS / 5; f++) {
 				GRect block = new GRect(BLOCK_WIDTH, BLOCK_HIGHT);
 				block.setColor(Color.CYAN);
 				block.setFilled(true);
 				block.setFillColor(Color.CYAN);
 				add(block, blockX, blockY);
 				numberOfBlocks++;
+				blocks[i][f] = block;
 				blockY += BLOCK_HIGHT + SPACE_BETWEEN_BLOCKS;
 			}
 			blockY = BLOCKS_TOP_OFFSET;
@@ -113,74 +131,107 @@ public class BreakoutGame extends GraphicsProgram {
 		}
 	}
 
-	private void createPaddle() {
+	private void createPaddle() {//создает доску
 		paddle.setFilled(true);
 		paddle.setFillColor(Color.BLACK);
 		add(paddle);
 	}
 
-	private void createCollider() {
-		int colliderX = (WINDOW_WIDTH - PADDLE_WIDTH) / 2 + PADDLE_WIDTH / 2
-				- COLLIDER_RADIUS;
-		int colliderY = WINDOW_HIGHT - PADDLE_BOTTOM_OFFSET - COLLIDER_RADIUS
-				* 2;
-		GOval collider = new GOval(COLLIDER_RADIUS * 2, COLLIDER_RADIUS * 2);
+	private void createCollider() {//создает шар
 		collider.setFilled(true);
 		collider.setFillColor(Color.BLACK);
 		add(collider, colliderX, colliderY);
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent e) {//слушатель нажатия клавиш(бессмыслено)
 
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		System.out.println(e.getKeyCode());
-		if (e.getKeyCode() == 32 && !field) {
-			createField();
+		System.out.println(e.getKeyCode());//проверяет, какая клавиша нажата(для удобства работы с кодом)
+		if (e.getKeyCode() == 32 && !field) {//проверяет нажатие пробела
+			createField();//создает поле
 			field = true;
-		}
-		if (e.getKeyCode() == 16){
-			startGame();
 			game = true;
+			startGame();//начинает саму игру
 		}
-			
 
 	}
 
-	private void startGame() {
-		
-		
+	private void startGame() {//начинает игру
+		while (game) {
+			moveCollider();//запускает Шар
+			checkForCollision();//проверяет удар
+			checkForBlocksLeft();//проверяет, очтались ли блоки
+		}
+	}
+
+	private void checkForBlocksLeft() {// проверяет на наличие блоков
+		if (numberOfBlocks < 1)
+		game = false;//завершает игру
+
+	}
+
+	private void checkForCollision() {
+		checkForCollisionX();//проверяет столкновение по X
+		checkForCollisionY();//проверяет столкновение по Y
+
+	}
+
+	private void checkForCollisionY() {
+		if (collider.getY() < 0) {
+			for (int i = 0; i < NUMBER_OF_COLOMNES; i++) {
+				for (int f = 0; f < NUMBER_OF_RAWS; f++) {
+					if (collider.equals(blocks[i][f])) {
+						remove(blocks[i][f]);//EXTERMINATE!
+						numberOfBlocks--;//
+						colliderDirectionY *= -1;//разворачивает Шар
+						break;
+					}
+				}
+			}
+			colliderDirectionY *= -1;
+
+		}
+
+	}
+
+	private void checkForCollisionX() {//смотреть checkForCollisionY
+		if (collider.getX() < 0
+				|| collider.getX() + COLLIDER_RADIUS * 2 > WINDOW_WIDTH) {
+			for (int i = 0; i < NUMBER_OF_COLOMNES; i++) {
+				for (int f = 0; f < NUMBER_OF_RAWS; f++) {
+					if (collider.equals(blocks[i][f])) {
+						remove(blocks[i][f]);
+						numberOfBlocks--;
+						colliderDirectionX *= -1;
+						break;
+					}
+				}
+			}
+			colliderDirectionX *= -1;
+		}
+	}
+
+	private void moveCollider() {//сила, движущая Шар
+		collider.move(-colliderDirectionX, -colliderDirectionY);
+		pause(SPEED);
+
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e) {//управление платформой
 		if (game && e.getKeyCode() == 37 && paddle.getX() > 0) {
-			paddle.move(-4, 0);
+			paddle.move(-SPEED, 0);
 			pause(SPEED);
 		}
 		if (game && e.getKeyCode() == 39
 				&& paddle.getX() + PADDLE_WIDTH < WINDOW_WIDTH) {
-			paddle.move(4, 0);
-			pause(SPEED);
+			paddle.move(SPEED, 0);
+
 		}
-
-	}
-
-	private void blockCollision() {
-		// TODO EGOR check for collision between Collider and Block
-
-	}
-
-	private void colliderDirectionY() {
-		// TODO create boolean to upward-downward direction
-
-	}
-
-	private void colliderDirectionX() {
-		// TODO create booleans for right-left direction
 
 	}
 
